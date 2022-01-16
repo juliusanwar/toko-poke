ba172c3043fc9040221209abe8a7ea419bb6d73f
Julius Anwar <julius.anwar@live.com>
2022-01-16 23:19:39 +0700

    Revert "Update Main"

---
+++ pokedex/src/api/axios.ts
@@ -0,0 +1,32 @@
+import Axios from "axios";
+import { HTTP_METHODS } from "../globals";
+
+const axios = Axios.create({
+  baseURL: "https://pokeapi.co/api/v2",
+});
+
+export const createApiRequest = async (
+  url: string,
+  method: HTTP_METHODS,
+  data: any
+) => {
+  try {
+    const response = await axios({
+      url,
+      method,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      data,
+    });
+    return response.data;
+  } catch (err: any) {
+    console.error(err);
+    throw new Error(err);
+  }
+};
+export const baseImageUrl =
+  "https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/";
+
+export default axios;
