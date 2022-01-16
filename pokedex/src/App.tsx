a44b09f5e2a999479eaa89a488360916963a78c2
Julius Anwar <julius.anwar@live.com>
2022-01-16 21:55:44 +0700

    Pokedex Pedia v1

--- pokedex/src/App.tsx
+++ pokedex/src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import SplashScreen from "./components/SplashScreen";
+import {
+  cachedPokemonsSelector,
+  getCachedPokemons,
+} from "./features/cachedPokemonsSlice";
 
-function App() {
+import { SliceStatus } from "./globals";
+import Routes from "./Routes";
+
+const App: React.FC = () => {
+  const dispatch = useDispatch();
+  const cachedPokemons = useSelector(cachedPokemonsSelector);
+
+  useEffect(() => {
+    dispatch(getCachedPokemons());
+    //eslint-disable-next-line
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {cachedPokemons.status.state === SliceStatus.LOADING ||
+        cachedPokemons.status.state === SliceStatus.IDLE ? (
+        <SplashScreen />
+      ) : (
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+      )}
+    </>
   );
-}
+};
 
 export default App;
--- pokedex/src/index.tsx
+++ pokedex/src/index.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { HelmetProvider, Helmet } from "react-helmet-async";
+import { Provider } from "react-redux";
+import store from "./features/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>React Pokédex</title>
+        <meta
+          name="description"
+          content="a simple pokédex for your pokemon needs."
+        />
+      </Helmet>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </HelmetProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
